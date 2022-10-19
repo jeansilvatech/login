00543f68697b85f5dd7bcc874dace5cd79686107
Jean Silva <jeanpsilva10@gmail.com>
2022-10-19 17:51:22 -0300

    Adicionando form de cadastro

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const btnRegistration = document.querySelector('.btn-registration')
+const btnBack = document.querySelector('.btn-back')
+const formLogin = document.querySelector('.login')
+const formRegistration = document.querySelector('.registration')
+
+btnRegistration.addEventListener('click', ()=>{
+    formLogin.style.display = "none"
+    formRegistration.style.display = "flex"
+})
+btnBack.addEventListener('click', ()=>{
+    formLogin.style.display = "flex"
+    formRegistration.style.display = "none"
+    console.log('clicou')
+})
