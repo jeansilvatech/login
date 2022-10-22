1837d9ab1717b26304c751dd2e963b12f7889f20
Jean Silva <jeanpsilva10@gmail.com>
2022-10-22 10:52:35 -0300

    Atualizando estilização

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ const formRegistration = document.querySelector('.registration')
 btnRegistration.addEventListener('click', ()=>{
     formLogin.style.display = "none"
     formRegistration.style.display = "flex"
+    document.title = "Cadastrar"
 })
 btnBack.addEventListener('click', ()=>{
     formLogin.style.display = "flex"
     formRegistration.style.display = "none"
     console.log('clicou')
+    document.title = "Login"
 })
